904c4dba5e5135400e1454f80fe06bc8144b4d09
Nobuyoshi Nakada <nobu@ruby-lang.org>
2020-11-27 21:42:46 +0900

    Remove unnecessary Random::DEFAULT expectations
    The respond_to expectation just suffice as duck-typing.

--- spec/ruby/core/random/default_spec.rb
+++ spec/ruby/core/random/default_spec.rb
@@ -6,18 +6,6 @@ describe "Random::DEFAULT" do
     Random::DEFAULT.should respond_to(:rand)
   end
 
-  ruby_version_is ''...'3.0' do
-    it "returns a Random instance" do
-      Random::DEFAULT.should be_an_instance_of(Random)
-    end
-  end
-
-  ruby_version_is '3.0' do
-    it "refers to the Random class" do
-      Random::DEFAULT.should.equal?(Random)
-    end
-  end
-
   it "changes seed on reboot" do
     seed1 = ruby_exe('p Random::DEFAULT.seed', options: '--disable-gems')
     seed2 = ruby_exe('p Random::DEFAULT.seed', options: '--disable-gems')
