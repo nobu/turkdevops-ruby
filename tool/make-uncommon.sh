98814df02d7995adc07ffa572b42947e8be8c46a
Nobuyoshi Nakada <nobu@ruby-lang.org>
2023-01-13 17:40:14 +0900

    Move release date macros to uncommon.mk

--- tool/make-uncommon.sh
+++ tool/make-uncommon.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 cd "$1"
 
-exec sed 's/{\$([^(){}]*)[^{}]*}//g' common.mk
+sed '/^#/q' common.mk # mode-line
+for f in version.h revision.h; do
+    test -f "$f" || continue
+    sed -n \
+	-e '/^#define \(RUBY_RELEASE_[A-Z]*\) \([0-9][0-9]*\)/{' \
+	-e   's//\1 = \2/' \
+	-e   's/ \([0-9]\)$/ 0\1/' \
+	-e   p \
+	-e '}' "$f"
+done
+exec sed '1s/^#.*//;s/{\$([^(){}]*)[^{}]*}//g' common.mk
