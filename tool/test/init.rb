815a7b32b877a7bd5d43d5edd71bee7d5e6706fc
Nobuyoshi Nakada <nobu@ruby-lang.org>
2024-04-06 21:27:33 +0900

    Set GEM_TEST_TMPDIR to get rid of too long path

--- tool/test/init.rb
+++ tool/test/init.rb
@@ -2,6 +2,22 @@
 # Note that this file is loaded not only by test/runner.rb but also by tool/lib/test/unit/parallel.rb.
 
 ENV["GEM_SKIP"] = ENV["GEM_HOME"] = ENV["GEM_PATH"] = "".freeze
+tmpdir = ENV["TMPDIR"]
+if tmpdir
+  tmpdir = File.realpath(tmpdir) # TMPDIR must be an existing path.
+  tmpdir = nil if tmpdir.size > 50
+end
+tmpdir ||= gemtmpdir = (require 'tmpdir'; Dir.mktmpdir("gemtest.", "/tmp"))
+ENV["GEM_TEST_TMPDIR"] = tmpdir
+warn "GEM_TEST_TMPDIR: #{tmpdir.inspect}"
+if gemtmpdir
+  pid = $$
+  at_exit do
+    Dir.rmdir(gemtmpdir) if pid == $$
+  rescue SystemCallError
+  end
+end
+
 ENV.delete("RUBY_CODESIGN")
 
 Warning[:experimental] = false
