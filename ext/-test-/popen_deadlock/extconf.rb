97da6a3957eacd68c989b79c978d4451097f7ecd
Nobuyoshi Nakada <nobu@ruby-lang.org>
2024-11-07 13:15:30 +0900

    Clear autogenerated dependencies

--- ext/-test-/popen_deadlock/extconf.rb
+++ ext/-test-/popen_deadlock/extconf.rb
@@ -2,5 +2,9 @@
 case RUBY_PLATFORM
 when /solaris/i, /linux/i
   $LDFLAGS << " -ldl"
-  create_makefile("-test-/popen_deadlock/infinite_loop_dlsym")
+  link = true
+end
+create_makefile("-test-/popen_deadlock/infinite_loop_dlsym") do |mk|
+  mk.sub!(/^all: .*/, 'all: Makefile') unless link
+  mk
 end
